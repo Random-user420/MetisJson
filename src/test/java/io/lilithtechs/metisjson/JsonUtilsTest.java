b2f41c474a6bb81bc4c76ebcee64a564a5e7b326
Random-user420 <leon_schmid@tutanota.com>
2025-09-03 20:34:34 +0200

    typo

--- src/test/java/io/lilithtechs/metisjson/JsonUtilsTest.java
+++ src/test/java/io/lilithtechs/metisjson/JsonUtilsTest.java
@@ -13,7 +13,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
-package io.lilithtechs.metrisjson;
+package io.lilithtechs.metisjson;
 
 
 import java.util.ArrayList;
